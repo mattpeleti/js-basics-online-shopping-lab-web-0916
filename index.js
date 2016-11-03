bf69e728fba499f121cdcf119ef482ed3b6f1632
github username <github email address>
2016-11-03 17:24:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
+}
+
+function getCart(){
+  return cart
 }
 
 function total() {
@@ -14,4 +18,53 @@ function total() {
   }
 
   return t
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random() * 100)
+  cart.push({[item]: price})
+  console.log(`${item} has been added to your cart.`)
+  return getCart()
+}
+
+
+function viewCart(){
+  var string = `In your cart, you have `
+  if(cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  }else{
+    for (var cv = 0; cv < cart.length; cv++){
+      for (var itemName in cart[cv]){
+        string += `${itemName} at $${cart[cv][itemName]}`
+        if(cv < cart.length - 1){
+          string += ", "
+        }
+        else {
+          string += "."
+        }
+      }
+    }
+    console.log(string)
+  }
+}
+
+function removeFromCart(itemname){
+  for (var cv = 0; cv < cart.length; cv++){
+    if(Object.keys(cart[cv])[0] === itemname){
+      cart.splice(cv,1)
+      // return cart
+    }else {
+      // console.log("That item is not in your cart.") THIS SHOULD WORK BUT DOESN'T
+    }
+  }
+  console.log("That item is not in your cart.")
+}
+
+function placeOrder(cardNumber){
+  if(cardNumber){
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  }else{
+    console.log("We don't have a credit card on file for you to place your order.")
+  }
 }
